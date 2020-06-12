6f6ab7017b4a7095edb7f04a30203215d47c1da4
nat-alia <nvoledzka@gmail.com>
2020-06-11 23:54:23 -0400

    Mouse Interactions

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ $( document ).ready(function() {
     const studioBox = document.querySelector('div.homepage-bottomright-studio')
 
 
+
     var cursorMoves = true;
     var width = $(window).width();
     var height = $(window).height();
+    var coorX;
+    var coorY;
+    var storedCoorX;
+    var storedCoorY;
 
-    if (cursorMoves) {
-      $( document ).mousemove(function(event){
-        var coorX = event.pageX;
-        var coorY = event.pageY;
+
+    $(document).mousemove(function(event){
+        coorX = event.pageX;
+        coorY = event.pageY;
 
         if (cursorMoves) {
           bioBox.style.width = coorX + "px"
@@ -29,19 +34,47 @@ $( document ).ready(function() {
 
           studioBox.style.width = width - coorX + "px"
           studioBox.style.height = height - coorY + "px"
-        }
+          $("*").css("cursor", "pointer");
 
-        $(document).click(function(){
-          cursorMoves = !cursorMoves;
-          })
 
+          $(document).click(function(){
+            cursorMoves = false;
+            storedCoorX = coorX;
+            storedCoorY = coorY;
+          });
+        };
+
+
+
+        if(!cursorMoves){
+
+          if(storedCoorX - 25 < event.pageX && storedCoorX + 25 > event.pageX && storedCoorY - 25 < event.pageY && storedCoorY + 25 > event.pageY){
+              $("*").css("cursor", "pointer");
+          }else{
+              $("*").css("cursor", "default");
+          };
+
+          // $(document).click(function(){
+          //   if(storedCoorX - 15 < event.pageX && storedCoorX + 15 > event.pageX && storedCoorY - 15 < event.pageY && storedCoorY + 15 > event.pageY){
+          //     cursorMoves = true;
+          //   };
+          // });
+
+
+          if(storedCoorX - 25 < event.pageX && storedCoorX + 25 > event.pageX && storedCoorY - 25 < event.pageY && storedCoorY + 25 > event.pageY){
+              $(document).click(function(){
+                if(storedCoorX - 25 < event.pageX && storedCoorX + 25 > event.pageX && storedCoorY - 25 < event.pageY && storedCoorY + 25 > event.pageY){
+                  cursorMoves = true;
+                };
+            });
+          };
+
+         $('h1').hover(function(){
+            $("*").css("cursor", "pointer");
+          });
+
+        };
       });
-    };
-
-
-
-
-
 
 
 
